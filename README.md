bd2604769f2ffc4c1b72112229f6563fd3d0234c
Jorand <jorand@datagif.fr>
2015-11-30 18:28:14 +0100

    udpate v2

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # jSlider
+
+jQuery plugins to make simple slider.
+
+##How to use ?
+Link the JS file in HTML.
+
+Now you can use like this `$("section").jSlider();`.
+
+And add settings `$("section").jSlider({'loop': true});`.
--- jSlider.js
+++ jSlider.js
@@ -1,10 +1,16 @@
+/**
+* @package jSlider
+* @author Jorand Le Pape <http://www.github.com/Jorand>
+* @github Jorand <http://www.github.com/Jorand/jSlider>
+*/
 (function($) {
 
 	$.fn.jSlider = function(options) {
 
 		var defauts = {
 			'loop': true,
-			'duration': 500
+			'duration': 500,
+			'offsetLeft': 0
 		};
 
 		var parametres = $.extend(defauts, options);
@@ -14,30 +20,106 @@
 			var $element = $(this),
 				$slider = $element.find('.jSlider'),
 				$first = $slider.children().first(),
-				$controls = $element.find('.jSlider-control'),
-
+				$controls = $element.find('.jSlider-control, .jSlider-control-item'),
+				$counter = $element.find('.jSlider-counter'),
+				$total = $element.find('.jSlider-total'),
+				
 				childCount = $slider.children().length,
 				sliderWidth = $slider.width(),
-				childWidth = $slider.children().first().outerWidth(),
-				slideCount = Math.round(sliderWidth / childWidth),
+				childs = [],
+				slideCount = 0,
 
-				slideCounter = 1,
 				counter = 1;
-			
+
+			resize();
+
 			$controls.click(function(event) {
 				event.preventDefault();
 
-				slide($(this).hasClass('prev'));
+				if (!$(this).hasClass('hide')) {
+
+					slide($(this).hasClass('prev'));
+				}
 			});
+
+			if (parametres.keyEvent) {
+
+				$(document).keydown(function(e) {
+					var k = e.keyCode;
+					
+					if (
+						k >= 37 && k <= 40 // Is arrow keys
+						&& !e.ctrlKey && !e.altKey && !e.shiftKey && !e.metaKey // Not meta keys, keeping keyboard shortcuts safe
+						&& $('input:focus, select:focus').length === 0 // Not filling a form
+					)
+						e.preventDefault();
+					else
+						return;
+
+					if (k == 39) {
+						slide();
+					}
+
+					if (k == 37) {
+						slide(true);
+					}
+				
+				});
+			}
 
 			$(window).bind('resize', resize);
 			update();
 
 			function resize() {
 
+				var lastChildIndex = 0;
+
+				if (childs.length > 0) {
+
+					lastChildIndex = childs[counter][0].childIndex;
+				}
+
 				sliderWidth = $slider.width();
-				childWidth = $slider.children().first().outerWidth();
-				slideCount = Math.round(sliderWidth / childWidth);
+
+				childs = [];
+
+				var childSlideCount = 1;
+
+				$first.stop(1).css({'margin-left': 0 + 'px'});
+
+				$slider.children().each(function(i) {
+
+					var slideOffset = sliderWidth;
+
+					if (childs[childSlideCount] !== undefined) {
+						slideOffset = childs[childSlideCount][0].childOffset + sliderWidth;
+					}
+
+					var childOffset = $(this).position().left + $(this).outerWidth();
+
+					if (childOffset > slideOffset) {
+						childSlideCount++;
+					}
+
+					if (!childs[childSlideCount])
+						childs[childSlideCount] = [];
+
+					childs[childSlideCount].push({
+						childIndex: i,
+						childSlide: childSlideCount,
+						childOffset: $(this).position().left
+					});
+					
+				});
+
+				slideCount = childs.length-1;
+
+				if (counter > slideCount)
+					counter = slideCount;
+
+				var sliderPos = childs[counter] !== undefined ? childs[counter][0].childOffset : 0;
+
+				$first.stop(1).css({'margin-left': - sliderPos + 'px'});
 				
 				update();
 			}
@@ -47,14 +129,26 @@
 				if (!parametres.loop) {
 						
 					$controls.filter('.prev')[(counter === 1 ? 'add' : 'remove')+'Class']('hide');
-					$controls.filter('.next')[(counter >= childCount ? 'add' : 'remove')+'Class']('hide');
+					$controls.filter('.next')[(counter >= slideCount ? 'add' : 'remove')+'Class']('hide');
 				}
 				
 				/* En px */
 				//$first.stop(1).animate({'margin-left': -childWidth * (counter - 1) + 'px'}, parametres.duration);
 
 				/* En % */
-				$first.stop(1).animate({'margin-left': - ((counter - 1) / slideCount) * 100 + '%'}, parametres.duration);
+				//$first.stop(1).animate({'margin-left': - ((counter - 1) / slideCount) * 100 + '%'}, parametres.duration);
+
+				/* En px step */
+				var sliderPos = childs[counter] !== undefined ? childs[counter][0].childOffset : 0;
+
+				if (sliderPos > 0) {
+					sliderPos -= parametres.offsetLeft;
+				}
+
+				$first.stop(1).animate({'margin-left': - sliderPos + 'px'}, parametres.duration);
+				
+				$counter.text(counter);
+				$total.text(slideCount);
 
 			}
 
@@ -63,21 +157,19 @@
 				prev = prev === true;
 					
 				if (prev) {
-					counter -= slideCount;
+					counter --;
 				}
 				else {
-					counter += slideCount;
+					counter ++;
 				}
 
 				if (counter === 0) {
-					counter = parametres.loop ? childCount : 1;
+					counter = parametres.loop ? slideCount : 1;
 				}
 
-				if (counter > childCount) {
-					counter = parametres.loop ? 1 : childCount;
+				if (counter > slideCount) {
+					counter = parametres.loop ? 1 : slideCount;
 				}
-
-				slideCounter = counter / slideCount;
 
 				update();
 			}
